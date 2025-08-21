0e5136ab21d9f3cc2e9777c3d5c6b4aa2c11869f
siyun0520 <syun5520@naver.com>
2025-08-21 22:47:28 +0900

    오류메시지 수정

--- src/main/java/likelion/traditional_market/Receipt/Service/ReceiptService.java
+++ src/main/java/likelion/traditional_market/Receipt/Service/ReceiptService.java
@@ -10,15 +10,18 @@ import likelion.traditional_market.Receipt.Dto.ReceiptCheckResponse;
 import likelion.traditional_market.UserKeyIssue.Entity.User;
 import likelion.traditional_market.UserKeyIssue.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.*;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ReceiptService {
     private final ClovaOcrClient clovaOcrClient;
     private final ReceiptParser receiptParser;
@@ -67,11 +70,20 @@ public class ReceiptService {
 
             return ReceiptCheckResponse.fail("영수증 인식이 충분하지 않습니다. 재촬영해주세요.");
 
-        } catch (Exception e) {
-            return ReceiptCheckResponse.fail("OCR 처리 실패: " + e.getMessage());
+        }
+        catch (IllegalArgumentException e) {
+            log.warn("데이터 조회 실패: {}",e.getMessage());
+            return ReceiptCheckResponse.fail("요청 처리 중 오류 발생: "+e.getMessage());
+        }
+        catch(IOException e){
+            log.error("이미지 처리 중 오류 발생",e);
+            return ReceiptCheckResponse.fail("이미지 처리 중 오류 발생");
+        }
+        catch(Exception e){
+            log.error("알 수 없는 오류 발생",e);
+            return ReceiptCheckResponse.fail("OCR 처리 중 알 수 없는 오류 발생");
         }
     }
-
     @Transactional
     public ReceiptCheckResponse handleReceiptTest(
             String userKey, int missionId, ExtractedFields fields, List<String> keywordHits) {
